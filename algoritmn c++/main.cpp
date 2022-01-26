e6190c44cc5508bc726346ab6c046cfa72ae8c92
ojw <ohjiwooo@gmail.com>
2022-01-26 18:36:44 +0900

    Initial Commit

---
+++ algoritmn c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algoritmn c++
+//
+//  Created by 오지우 on 2022/01/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
